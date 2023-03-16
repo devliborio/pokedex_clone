303df22c0167c0453a964709d3830f1088713602
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-16 12:03:40 -0300

    Usando o axios para acessar os dados da Poke API onde está a lista de todos os pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <div id="app">
-
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'App'
-
-}
+  name: "App",
+  created: function () {
+    // Método que é chamado toda vez que o componente é carregado na aplicação.
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log(res.data.results); // Acessando o Objeto Data e o Array onde contem todos os Pokemon's da primeira geração.
+      })
+      .catch((err) => console.log(err));
+  },
+};
 </script>
 
 <style>
-
 </style>
