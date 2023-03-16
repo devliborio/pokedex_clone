38943551687e6a84bfc8260379c1872f4744ae20
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-16 15:00:53 -0300

    Usando axios para pegar os dados da Poke API e armazenando na variável pokemon

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,14 +1,49 @@
 <template>
+  <div>
+    <h1>{{ num }} {{ upper(name) }}</h1>
+    <small>{{ url }}</small>
 
- <div>
-   <h1>{{num}} {{upper(name)}}</h1>
-   <small>{{url}}</small>
- </div>
+    <div class="card">
+    <div class="card-image">
+        <figure class="image is-4by3">
+        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        </figure>
+    </div>
+    <div class="card-content">
+        <div class="media">
+            <div class="media-content">
 
+            </div>
+        </div>
+
+        <div class="content">
+
+        </div>
+  </div>
+</div>
+
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+
+    created: function(){
+        axios.get(this.url).then((res) => {
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+        })
+    },
+
+    data(){
+        return{
+            pokemon: {}
+        }
+    },
+
     props: {
         num: Number,
         name: String,
@@ -25,5 +60,4 @@ export default {
 </script>
 
 <style>
-
 </style>
