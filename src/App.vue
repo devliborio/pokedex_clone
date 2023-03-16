e2642011b056af716b42645abb4500aa48bd9373
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-16 12:23:28 -0300

    Armazenando os dados recebidos da Poke API na variável pokemons

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,15 @@ import axios from "axios";
 
 export default {
   name: "App",
-  created: function () {
-    // Método que é chamado toda vez que o componente é carregado na aplicação.
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-      .then((res) => {
-        console.log(res.data.results); // Acessando o Objeto Data e o Array onde contem todos os Pokemon's da primeira geração.
-      })
-      .catch((err) => console.log(err));
+    data() {
+    return {
+      pokemons: [] // Variável que vai receber
+    };
+  },
+  created: function () { // Método que é chamado toda vez que o componente é carregado na aplicação.
+    
+    // Acessando o Objeto Data e o Array onde contem todos os Pokemon's da primeira geração.
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then((res) => {this.pokemons = res.data.results;}).catch((err) => console.log(err));
   },
 };
 </script>
