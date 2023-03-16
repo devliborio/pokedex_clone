4ad78c5849c57b6f8100cfdd7c09d4961c5e9487
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-16 19:06:22 -0300

    Mostrando habilidade do pokemon e reorganizando estrutura

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -2,6 +2,10 @@
   <div id="pokemon">
     <div class="card">
 
+        <div class="media-content">
+            <p class="title is-4">{{ upper(name) }}</p>
+        </div>
+
         <div class="card-image">
             <figure>
                 <img :src="currentImg">
@@ -13,8 +17,14 @@
             <div class="media">
 
                 <div class="media-content">
-                    <p class="title is-4">{{ num }} - {{ upper(name) }}</p>
-                    <p class="subtitle is-5">{{ pokemon.type }}</p>
+                    <p class="title is-5">Tipo</p>
+                    <p class="subtitle is-5"> - {{ pokemon.type_one }}</p>
+                    <p class="subtitle is-5">{{ pokemon.type_two }}</p>
+                </div>
+
+                <div class="media-content">
+                    <p class="title is-5"> Habilidade </p>
+                    <p class="subtitle is-5"> - {{ pokemon.habilidade }}</p>
                 </div>
 
             </div>
@@ -35,10 +45,12 @@ export default {
 
     created: function(){
         axios.get(this.url).then((res) => {
-            this.pokemon.type = res.data.types[0].type.name; 
+            this.pokemon.type_one = res.data.types[0].type.name;
             this.pokemon.front = res.data.sprites.front_default; 
             this.pokemon.back = res.data.sprites.back_default;
+            this.pokemon.habilidade = res.data.abilities[0].ability.name;
             this.currentImg = this.pokemon.front;
+            console.log(res)
             // Uma observação importante é que as variáveis (type, front e back se forem cridas somente pela requisição do axios ela não será reativa no HTML então se você quiser reatividade você terá que definir ela dentro do método data(){}.
         })
     },
