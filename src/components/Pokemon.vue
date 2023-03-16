1b4df1e15570d03e7b3ad1f1597351e9e3312927
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-16 13:25:39 -0300

    Criando lógica para deixar todos os nomes do Pokemon com letra maiúscula

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,8 +1,8 @@
 <template>
 
  <div>
-   <h1>{{ num }} {{ name }}</h1>
-   <small>{{ url }}</small>
+   <h1>{{num}} {{upper(name)}}</h1>
+   <small>{{url}}</small>
  </div>
 
 </template>
@@ -13,6 +13,13 @@ export default {
         num: Number,
         name: String,
         url: String
+    },
+
+    methods: { // Agora os filtros podem ser usado como métodos normais
+        upper: function (value) {
+            let newName = value[0].toUpperCase() + value.slice(1); 
+            return newName;
+        }    
     }
 }
 </script>
